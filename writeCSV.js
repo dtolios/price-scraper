4421df8c30e6d3d33313bb5001fbad7fb4ba9282
Dimitri Tolios <dtolios@riotgames.com>
2017-05-24 15:22:54 -0700

    Added documentation to writeCSV.js

--- writeCSV.js
+++ writeCSV.js
@@ -1,10 +1,15 @@
+/** @requires fs */
 const fs = require('fs');
 
+/** @requires csv */
 const csv = require('csv');
 
-// Check for a folder called 'data'
-// if the folder doesn't exist, create one
-// else do nothing
+/**
+ * @funciton saveData
+ * @param dirPath - the directory path you would like to create, and store your data in
+ * @param data - a javascript array of data
+ * Check for a folder called 'data', if the folder doesn't exist, create one, else do nothing
+ */
 function saveData(dirPath, data) {
     try {
         fs.mkdirSync(dirPath);
@@ -19,7 +24,13 @@ function saveData(dirPath, data) {
 
 }
 
-function makeCSV(data) {
+/**
+ * @function makeCSV
+ * @param dirPath - the directory path to pass to writeFile
+ * @param data - a javascript array of data
+ * Creates a CSV based on the data array. Calls writeFile if data is stringified successfully
+ */
+function makeCSV(dirPath, data) {
     const options = {
         columns: {
             title: 'Title',
@@ -32,13 +43,19 @@ function makeCSV(data) {
     };
     csv.stringify(data, options, (error, output) => {
         if(error) throw error;
-        writeFile(output);
+        writeFile(dirPath, output);
     })
 }
 
-function writeFile(data) {
+/**
+ * @function writeFile
+ * @param dirPath - the directory path to save data file
+ * @param data - a string of comma separated data
+ * Takes in the directory path and the data string, and writes to a file named YYYY-MM-DD.csv
+ */
+function writeFile(dirPath, data) {
     const date = new Date();
-    const fileName = `./data/${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}.csv`
+    const fileName = `${dirPath}/${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}.csv`
     fs.writeFile(fileName, data, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
